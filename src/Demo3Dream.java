cdbabb8ccc32f51b36d68eecac490ff725435402
mahongwei <mahongwei@qq.com>
2019-01-02 23:15:50 +0800

    first!

---
+++ src/Demo3Dream.java
@@ -0,0 +1,7 @@
+public class Demo3Dream {
+    public static void main(String[] args) {
+        System.out.println("how!");
+        System.out.println("are");
+        System.out.println("you!");
+    }
+}
